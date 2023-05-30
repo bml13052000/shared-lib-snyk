8c8c00d88410092354a47e8da6b14278911b509e
bml13052000 <baivabmaulik1320@gmail.com>
2023-05-30 11:12:45 +0530

    Error9 fixed in otherAgent.groovy

--- vars/otherAgent.groovy
+++ vars/otherAgent.groovy
@@ -3,7 +3,7 @@ def call(def name){
     parallel(
         hello: {
             stage("Say hello"){
-
+             git branch: 'main', url: 'https://github.com/KmartAU/devsecops-webgoat-test.git',credentialsId: 'fe60baad-0061-4b8c-8851-29b413ba9b3e'
             sh "hostname"
             sh "echo hello ${name}"
   
