8d4a45bc2b9cb485c82b958cb73a73630794aea3
bml13052000 <baivabmaulik1320@gmail.com>
2023-05-30 13:23:13 +0530

    Renaming otheragent.groovy

--- vars/Snyk.groovy
+++ vars/Snyk.groovy
@@ -5,7 +5,10 @@ def call(def name){
             stage("Say hello"){
              git branch: 'main', url: 'https://github.com/KmartAU/devsecops-webgoat-test.git',credentialsId: 'fe60baad-0061-4b8c-8851-29b413ba9b3e'
              sh "hostname"
-             sh "echo hello ${name}"
+            //  sh "echo hello ${name}"
+            sh "snyk auth ${token}"
+            sh "snyk test --all-projects"
+            sh "snyk monitor --all-projects"
   
             }
         }
