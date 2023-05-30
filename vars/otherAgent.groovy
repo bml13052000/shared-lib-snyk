68f27f080e58f4b7ebe4de0265942a5a57b0a529
bml13052000 <baivabmaulik1320@gmail.com>
2023-05-30 11:13:36 +0530

    Error10 fixed in otherAgent.groovy

--- vars/otherAgent.groovy
+++ vars/otherAgent.groovy
@@ -4,8 +4,8 @@ def call(def name){
         hello: {
             stage("Say hello"){
              git branch: 'main', url: 'https://github.com/KmartAU/devsecops-webgoat-test.git',credentialsId: 'fe60baad-0061-4b8c-8851-29b413ba9b3e'
-            sh "hostname"
-            sh "echo hello ${name}"
+             sh "hostname"
+             sh "echo hello ${name}"
   
             }
         }
