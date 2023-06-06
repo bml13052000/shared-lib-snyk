5739f14ba00243e2db18981030dd3608503eb72a
bml13052000 <baivabmaulik1320@gmail.com>
2023-06-06 21:15:59 +0530

    Update SnykIAC.groovy

--- vars/SnykIAC.groovy
+++ vars/SnykIAC.groovy
@@ -3,7 +3,7 @@ def call(def token){
     parallel(
         snykIac: {
             stage("Snyk IAC test"){
-             git branch: 'main', url: 'https://github.com/KmartAU/cfngoat.git',credentialsId: 'fe60baad-0061-4b8c-8851-29b413ba9b3e'
+             git branch: 'master', url: 'https://github.com/KmartAU/cfngoat.git',credentialsId: 'fe60baad-0061-4b8c-8851-29b413ba9b3e'
              sh "hostname"
             //  sh "echo hello ${name}"
             sh "snyk auth ${token}"
